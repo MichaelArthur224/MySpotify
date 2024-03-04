dc7835df5a97192d1e2e73cd2ad21159bddcd58f
Michael Arthur <87254861+MichaelArthur224@users.noreply.github.com>
2024-03-04 13:19:16 -0800

    login.js

--- src/login.js
+++ src/login.js
@@ -3,7 +3,7 @@ import './login.css'
 
 
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=97a1a89c45a34132a37aaa2397248ea4&response_type=code&redirect_uri=http://localhost:3000&scope=user-top-read%20user-read-email%20user-read-private%20user-library-read"
+  "https://accounts.spotify.com/authorize?client_id=YOUR_ID_HERE&response_type=code&redirect_uri=http://localhost:3000&scope=user-top-read%20user-read-email%20user-read-private%20user-library-read"
 
   export default function Login() {
     return (
